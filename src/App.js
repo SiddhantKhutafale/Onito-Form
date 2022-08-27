a13cfabf6a06f2bf82506ce02d17db1c2d2cbae4
Siddhant khutafale <100528991+SiddhantKhutafale@users.noreply.github.com>
2022-08-27 15:28:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Multipleinputs from './component/forms/multipleinputs';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Multipleinputs/>
+
+    </div>
+  );
+}
+
+export default App;
